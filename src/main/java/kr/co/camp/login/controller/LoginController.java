01f99cb59a9674dfa24abad7d6748e737077863b
dao <dao@naver.com>
2017-03-03 17:57:30 +0900

    설정

--- src/main/java/kr/co/camp/login/controller/LoginController.java
+++ src/main/java/kr/co/camp/login/controller/LoginController.java
@@ -1,5 +1,20 @@
 package kr.co.camp.login.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.camp.repository.vo.MemberVO;
+
+@Controller
+@RestController
+@RequestMapping("/login")
 public class LoginController {
 
+	@RequestMapping("/login.do")
+	public void login(MemberVO member) {
+		
+		
+	}
+	
 }
