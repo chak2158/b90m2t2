15dd6ae3b840d9e4b42a03adeb6569c4908a69be
chak2156 <chak2156@gmail.com>
2017-03-09 20:32:45 +0900

    메인페이지와 컨텐츠들 커넥트

---
+++ src/main/webapp/html/js/loginVariables.js
@@ -0,0 +1,3 @@
+var member;
+var auth2;
+var path ="/b90m2t2/html/";
--- src/main/webapp/html/js/main/movingForm.js
+++ src/main/webapp/html/js/main/movingForm.js
@@ -1,3 +1,35 @@
 function goMainForm(){
-	$("div#content").load("search.html")
+	$("div#content").load(path+"/main/search.html");
 }
+function goLoginForm(){
+	$("div#content").load(path+"/login/loginForm.html");
+}
+function gologOut() {
+	localStorage.removeItem("member");
+	$("#logOutNav").addClass("hidden");
+	$("#LoginFormNav").removeClass("hidden");
+	$("#MyPageNav").addClass("hidden");
+	$("#RegisterNav").removeClass("hidden");
+	$("#content").load(path+"/main/search.html");
+	alert("로그아웃 하셨습니다.");
+}
+function goRegiAgreeForm(){
+	$("div#content").load(path+"/login/registerAgreeForm.html")
+}
+
+function goReview(){
+	$("div#content").load(path+"/board/list.html");
+}
+function goMyPage(){
+	$("div#content").load(path+"myPage/fullCalendar.html");
+}
+function goLoginForm(){
+	$("div#content").load(path+"/login/loginForm.html")
+}
+	
+function goRegister(){
+	$("div#content").load(path+"/login/registerForm.html")
+	
+}
+
+ 
