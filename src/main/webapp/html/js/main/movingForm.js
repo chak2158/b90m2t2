e08751c2f7389007a687bbb2d5d63b359c45f42f
chak2156 <chak2156@gmail.com>
2017-03-10 09:47:15 +0900

    개인작업물 통합

--- src/main/webapp/html/js/main/movingForm.js
+++ src/main/webapp/html/js/main/movingForm.js
@@ -1,8 +1,8 @@
 function goMainForm(){
-	$("div#content").load(path+"/main/search.html");
+	$("div#content").load(path+"main/search.html");
 }
 function goLoginForm(){
-	$("div#content").load(path+"/login/loginForm.html");
+	$("div#content").load(path+"login/loginForm.html");
 }
 function gologOut() {
 	localStorage.removeItem("member");
@@ -10,26 +10,22 @@ function gologOut() {
 	$("#LoginFormNav").removeClass("hidden");
 	$("#MyPageNav").addClass("hidden");
 	$("#RegisterNav").removeClass("hidden");
-	$("#content").load(path+"/main/search.html");
+	$("#content").load(path+"main/search.html");
 	alert("로그아웃 하셨습니다.");
 }
 function goRegiAgreeForm(){
-	$("div#content").load(path+"/login/registerAgreeForm.html")
+	$("div#content").load(path+"login/registerAgreeForm.html")
 }
 
 function goReview(){
-	$("div#content").load(path+"/board/list.html");
+	$("div#content").load(path+"board/list.html");
 }
 function goMyPage(){
 	$("div#content").load(path+"myPage/fullCalendar.html");
 }
 function goLoginForm(){
-	$("div#content").load(path+"/login/loginForm.html")
+	$("div#content").load(path+"login/loginForm.html")
 }
 	
-function goRegister(){
-	$("div#content").load(path+"/login/registerForm.html")
-	
-}
 
  
